2cad46abd859792ccd7b24ac5350c9ad01654da1
Michael Cotterell <mepcotterell@gmail.com>
2019-10-30 18:06:20 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eH
+
+clear
+figlet "BOO!"
+read -n 1 -s -r -p "Press any key to continue..."
+
+clear
+figlet "cs1302-ceP4"
+echo "This class exercise is designed to give you additional opportunities to work on your project and ask questions to the instructional staff."
+echo ""
+echo "You may form groups for the purposes of filling out a Class Exercise Checkpoint Slip (slip), however, please keep the following in mind:"
+echo ""
+echo "* You must work on your project individually per the policies in the syllabus."
+echo ""
+echo "* If you leave early, then you must cross out your name on your slip. No exceptions for any reason."
+echo ""
+echo "* No checkpoint signatures will be given for this exerise, however, slips will be collected at the end of class. Furthermore, slips will only be accepted at the end of class."
+echo ""
+read -n 1 -s -r -p "Press any key to continue..."
+echo ""
+echo ""
+echo " .-."
+echo "(o o) boo!"
+echo "| O \\"
+echo " \   \\"
+echo "  \`~~~'"
+echo ""
