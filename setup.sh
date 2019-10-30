8e1ad1246bd5ab7d078701937e73f25c7cb1da91
Michael Cotterell <mepcotterell@gmail.com>
2019-10-30 18:14:07 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,8 @@
 
 clear
 figlet "BOO!"
-read -n 1 -s -r -p "Press any key to continue..."
+echo ""
 
-clear
 figlet "cs1302-ceP4"
 echo "This class exercise is designed to give you additional opportunities to work on your project and ask questions to the instructional staff."
 echo ""
@@ -15,9 +14,6 @@ echo ""
 echo "* If you leave early, then you must cross out your name on your slip. No exceptions for any reason."
 echo ""
 echo "* No checkpoint signatures will be given for this exerise, however, slips will be collected at the end of class. Furthermore, slips will only be accepted at the end of class."
-echo ""
-read -n 1 -s -r -p "Press any key to continue..."
-
 echo ""
 echo ""
 echo " .-."
